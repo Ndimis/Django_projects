42e92fded5380b6d11af962a19e200f37245c9b5
Thierry Ndimis <tndimis@live.fr>
2023-07-04 23:46:06 -0400

    Ajout des vue et vue detailées pour le R du CRUD

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,17 +3,25 @@ from django.http import HttpResponse
 from listings.models import Band, Listing
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', {'bands': bands})
+    return render(request, 'listings/brand_list.html', {'bands': bands})
+
+def band_detail(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request, 'listings/brand_detail.html', {'band': band})
     
 
 def about(request):
     return render(request, 'listings/about.html')
 
-def listings(request):
+def listing_list(request):
     listings = Listing.objects.all()
-    return render(request, 'listings/listing.html', {'listings': listings})
+    return render(request, 'listings/listing_list.html', {'listings': listings})
+
+def listing_detail(request, list_id):
+    listing = Listing.objects.get(id=list_id)
+    return render(request, 'listings/listing_detail.html', {'listing': listing})
     
 
 def contact(request):
