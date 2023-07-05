8aa8d62ba45bda8b696426dac04bafe2ff91f857
Thierry Ndimis <tndimis@live.fr>
2023-07-05 03:03:54 -0400

    Ajout des fonctionnalités CUD

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from listings.models import Band, Listing
-from listings.forms import  ContactUsForm
+from listings.forms import  ContactUsForm, BandForm, ListingForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
 
 def band_list(request):
@@ -11,10 +14,42 @@ def band_list(request):
 def band_detail(request, band_id):
     band = Band.objects.get(id=band_id)
     return render(request, 'listings/brand_detail.html', {'band': band})
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save() 
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+    return render(request, 'listings/band_create.html', {'form': form})
+
+def band_update(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == 'POST':
+        form = BandForm(request.POST,instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request, 'listings/band_update.html', {'form': form})
+
+def band_delete(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
     
+    return render(request, 'listings/band_delete.html', {'band': band})
 
 def about(request):
     return render(request, 'listings/about.html')
+
+def email_send(request):
+    return render(request, 'listings/email_send.html')
 
 def listing_list(request):
     listings = Listing.objects.all()
@@ -26,7 +61,50 @@ def listing_detail(request, list_id):
     
 
 def contact(request):
-    form = ContactUsForm()  # ajout d’un nouveau formulaire ici
+
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST) 
+        if form.is_valid():
+            send_mail(
+            subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['tndimis@gmail.com'],
+        )
+        return redirect('email-sent')
+    else:
+        form = ContactUsForm()
+    
     return render(request,
           'listings/contact.html',
           {'form': form})  
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save() 
+            return redirect('band-detail', listing.id)
+    else:
+        form = ListingForm()
+    return render(request, 'listings/listing_create.html', {'form': form})
+
+
+def listing_update(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('listing-detail', kwargs={'list_id': listing.id}))
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(request, 'listings/listing_update.html', {'form': form, 'listing':listing})
+
+def listing_delete(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    return render(request, 'listings/listing_delete.html', {'listing': listing })
